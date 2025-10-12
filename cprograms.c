049b05c4470f98da15f0de15ca5f26304fba2c6c
Arjun252gs <120630691+Arjun252gs@users.noreply.github.com>
2025-10-12 16:01:51 +0530

    Update cprograms.c
    Celsius to Fahrenheit conversion

--- cprograms.c
+++ cprograms.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int age;
-    printf("Enter the age ");
-    scanf("%d", &age);
-    printf("User age is %d", age);
+    int cel,far;
+    printf("Enter the celsius ");
+    scanf("%d", &cel);
+    far=cel*9/5+32;
+    printf("Fahrenheit value is %d", far);
 }
+//Refer - https://onecompiler.com/c/43ze3pwub
