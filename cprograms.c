5469703e3bcb6d0e0b47966722878e029c384b10
Arjun252gs <120630691+Arjun252gs@users.noreply.github.com>
2025-10-12 16:07:03 +0530

    Update cprograms.c
    Converting Fahrenheit to Celsius as well in the same program.

--- cprograms.c
+++ cprograms.c
@@ -5,6 +5,12 @@ int main()
     printf("Enter the celsius ");
     scanf("%f", &cel);
     far=cel*9/5+32;
-    printf("Fahrenheit value is %f", far);
+    printf("Fahrenheit value is %f\n", far);
+    cel=0;
+    far=0;
+    printf("Enter the Fahrenheit ");
+    scanf("%f", &far);
+    cel=(far-32)*5/9;
+    printf("Celsius value is %f", cel);  
 }
-//Refer - https://onecompiler.com/c/43ze3xwr9
+//Refer - https://onecompiler.com/c/43ze42uey
