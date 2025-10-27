030faba35863dfdeeb03a6ea79e57d87df30227e
Arjun252gs <120630691+Arjun252gs@users.noreply.github.com>
2025-10-27 11:27:48 +0530

    Update lab_programs.c

--- lab_programs.c
+++ lab_programs.c
@@ -74,16 +74,43 @@ int main()
 	return 0;
 } //Refer - https://onecompiler.com/c/43zsz6mzg
 
+//Program - 4 : Question - Generate Floyd’s triangle for given rows.
 
-//Program - 4 : Question - 
-//Program - 5 : Question - 
-//Program - 6 : Question - 
-//Program - 7 : Question - 
-//Program - 8 : Question - 
-//Program - 9 : Question - 
-//Program - 10 : Question - 
-//Program - 11 : Question - 
-//Program - 12 : Question - 
-//Program - 13 : Question - 
-//Program - 14 : Question - 
+#include<stdio.h>
+int main()
+{	
+  	int num;
+  	int i,j,k=1;
+  	
+  	printf("Enter the number:\n");
+  	scanf("%d",&num);
+  
+  	for(i=1; i<=num; i++)
+  	{
+    		for(j=1; j<=i; j++)
+    		{
+    			printf("%d\t",k++);
+    		}
+    		printf("\n");
+  	}
+  	return 0;
+} // Refer - https://onecompiler.com/c/442uryfpw
+
+//Program - 5 : Question - Develop a program to find key elements in an array using linear search.
+
+
+//Program - 6 : Question - Develop a program to find the roots of quadratic equations.
+
+//Program - 7 : Question - Develop a program to concatenate two strings, find length of a string and copy one string to other
+using string operations.
+
+//Program - 8 : Question - Develop a program to find whether a given number is prime or not.
+//Program - 9 : Question - Develop a modular program to find GCD and LCM of given numbers
+//Program - 10 : Question - Develop a program to add two numbers using the pointers to the variables.
+//Program - 11 : Question - Develop a program to find the transpose of a matrix.
+//Program - 12 : Question - Develop a program to perform Matrix Multiplication.
+//Program - 13 : Question - Develop a program to declare the structure of employees and display the employee records with
+higher salary among two employees.
+//Program - 14 : Question - Develop a program to create an array of structures to store book details and check whether a specific
+book, as requested by the user, is available or not.
 
